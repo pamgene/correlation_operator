77ce134e024ac3b2ab7dfc2232a05ea712c680b4
amaurel <alexandre.maurel@gmail.com>
2018-01-18 10:58:24 +0100

    init

---
+++ main.R
@@ -0,0 +1,32 @@
+library(tercen)
+library(dplyr)
+ 
+do.pearson = function(df, ...){
+  
+  result = try(cor.test(df$.x,df$.y, ...), silent = TRUE)
+  
+  if(inherits(result, 'try-error')) {
+    return (data.frame(.ri = integer(),
+                       .ci = integer(),
+                       cor=double(),
+                       pv=double()))
+  }  
+  
+  return (data.frame(.ri = df$.ri[1],
+                     .ci = df$.ci[1],
+                     cor=result$estimate,
+                     pv=result$p.value))
+}
+
+ctx = tercenCtx() 
+
+if (!(ctx$hasNumericXAxis)) stop('A numeric x axis is required')
+
+ctx %>% 
+  select(.ci, .ri, .x, .y) %>% 
+  group_by(.ci, .ri) %>%
+  do(do.pearson(., method="pearson")) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
+
+
