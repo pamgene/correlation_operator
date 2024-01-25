40aaa0557274ddec7067c3e4d111356de5ade702
agouy <alexandre.gouy@tercen.com>
2024-01-25 13:24:41 +0000

    Added GH workflows and support for different methods

--- main.R
+++ main.R
@@ -1,31 +1,33 @@
 library(tercen)
 library(dplyr)
  
-do.pearson = function(df, ...){
+do.cortest = function(df, ...) {
   
   result = try(cor.test(df$.x,df$.y, ...), silent = TRUE)
   
   if(inherits(result, 'try-error')) {
     return (data.frame(.ri = integer(),
                        .ci = integer(),
-                       cor=double(),
-                       pv=double()))
+                       cor_coef = double(),
+                       p_value = double()))
   }  
   
   return (data.frame(.ri = df$.ri[1],
                      .ci = df$.ci[1],
-                     cor=result$estimate,
-                     pv=result$p.value))
+                     cor_coef = result$estimate,
+                     p_value = result$p.value))
 }
 
 ctx = tercenCtx() 
 
 if (!(ctx$hasNumericXAxis || ctx$isPairwise)) stop('A numeric x axis is required')
 
+method <- ctx$op.value("method", as.character, "pearson")
+
 ctx %>% 
   select(.ci, .ri, .x, .y) %>% 
   group_by(.ci, .ri) %>%
-  do(do.pearson(., method="pearson")) %>%
+  do(do.cortest(., method = method)) %>%
   ctx$addNamespace() %>%
   ctx$save()
 
